8ef927f1a83d0caf7767e394ca93e6910ec95c33
Jeongmin Byun <jmbyun91@gmail.com>
2019-09-30 12:22:30 +0900

    replace links in the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,18 +3,22 @@
     <div class="container">
       <div class="footer-inner">
         <div class="footer-item">
-          <div class="text">
+          <div class="footer-text">
             &copy; Copyright {{(new Date()).getFullYear()}} | Users &amp; Information Lab.
           </div>
-          <div class="text">
+          <div class="footer-text">
             U&I Lab., Room 4417, E3-1 Computer Science Building,<br />
             KAIST, 291 Daehak-ro, Yuseong-gu, Daejeon 34141, South Korea
           </div>
         </div>
         <div class="footer-item">
-          <div class="text">
+          <div class="footer-text">
             <a href="https://github.com/uilab-github/uilab-github.github.io">
-              Admin
+              Repository for this Website
+            </a>
+            &nbsp;|&nbsp;
+            <a href="https://docs.google.com/spreadsheets/d/1v2JBn1EHa20kcwz5rtszoNmjx9Znqzk-dGMhKw1m3vE/edit?usp=sharing">
+              Edit Content
             </a>
           </div>
         </div>
@@ -32,7 +36,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .footer {
   background-color: var(--text-color);
   color: var(--light-grey-color);
@@ -48,15 +52,24 @@ export default {
 .footer-item {
   flex: 0 0 auto;
 }
-.text {
+.footer-text {
   padding: 0.2rem 0;
   font-size: 0.7rem;
 }
-.text a {
+.footer-text a {
   text-decoration: none;
   color: var(--light-grey-color);
 }
-.text a:hover {
+.footer-text a:hover {
+  text-decoration: underline;
   color: var(--white-color);
+}
+@media screen and (max-width: 48rem) {
+  .footer-inner {
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 }
 </style>
