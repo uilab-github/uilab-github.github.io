1df9897c900e458588b20f53c507e9163b49a114
Dongkwan Kim <todoaskit@gmail.com>
2022-04-26 15:08:45 +0900

    Add latest demos

--- src/components/LatestPublications.vue
+++ src/components/LatestPublications.vue
@@ -3,7 +3,17 @@
     <h3 class="row-title">Latest Publications</h3>
     <template v-if="research.length > 0">
       <research-item 
-        v-for="(item, i) in research[0].items.slice(0, listSize)" 
+        v-for="(item, i) in research[0].items.slice(0, pubListSize)" 
+        :key="i" 
+        :item="item" 
+        :tags="tags" 
+        :activeTagId="null" 
+      />
+    </template>
+    <h3 class="row-title row-not-first">Latest Demos</h3>
+    <template v-if="research.length > 0">
+      <research-item 
+        v-for="(item, i) in research[1].items.slice(0, demoListSize)" 
         :key="i" 
         :item="item" 
         :tags="tags" 
@@ -19,7 +29,8 @@
 <script>
 import ResearchItem from '@/components/ResearchItem.vue'
 
-const LIST_SIZE = 7
+const PUB_LIST_SIZE = 6
+const DEMO_LIST_SIZE = 3
 
 export default {
   name: 'research-category',
@@ -32,13 +43,17 @@ export default {
   },
   data() {
     return {
-      listSize: LIST_SIZE
+      pubListSize: PUB_LIST_SIZE,
+      demoListSize: DEMO_LIST_SIZE,
     }
   }
 }
 </script>
 
 <style scoped>
+.row-not-first {
+  padding-top: 2.5rem;
+}
 .link-buttons {
   padding-top: 2.5rem;
 }
